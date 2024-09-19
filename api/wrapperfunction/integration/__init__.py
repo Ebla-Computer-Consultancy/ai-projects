de440c319493c57d211bcbb4e9c8ca1ce6afdecb
alaa-alnissany <64522552+alaa-alnissany@users.noreply.github.com>
2024-09-19 15:57:07 +0300

    first version

--- api/wrapperfunction/integration/__init__.py
+++ api/wrapperfunction/integration/__init__.py
@@ -3,4 +3,5 @@ from . import openai_connector as openaiconnector
 from . import storage_connector as storageconnector
 from . import speech_connector as speechconnector
 from . import avatar_connector as avatarconnector
-__all__ = ["aisearchconnector", "openaiconnector","storageconnector","speechconnector","avatarconnector"]
+from . import crawl_integration as crawlintegration
+__all__ = ["aisearchconnector", "openaiconnector","storageconnector","speechconnector","avatarconnector","crawlintegration"]
