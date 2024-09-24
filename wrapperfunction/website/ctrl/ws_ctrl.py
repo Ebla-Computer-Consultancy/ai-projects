f9be06d3d4faf5d74a9d221c0ad7451c557e84b9
MhamadKab96 <141550928+MhamadKab96@users.noreply.github.com>
2024-09-24 13:53:09 +0300

    adding endpoint to get chat history

--- wrapperfunction/website/ctrl/ws_ctrl.py
+++ wrapperfunction/website/ctrl/ws_ctrl.py
@@ -1,11 +1,16 @@
 from wrapperfunction.website.model.search_criterial import searchCriteria
 from wrapperfunction.website.model.chat_payload import ChatPayload
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 import wrapperfunction.website.service.ws_service as wsservice
+from wrapperfunction.database.cosmos_connector import CosmosDBConnector
 from typing import List
 
 router = APIRouter()
 
+<<<<<<< HEAD:wrapperfunction/website/ctrl/ws_ctrl.py
+=======
+db_connector = CosmosDBConnector()
+>>>>>>> 4a5de49 (adding endpoint to get chat history):api/wrapperfunction/website/ctrl/ws_ctrl.py
 
 @router.post("/search")
 def search(rs: searchCriteria):
@@ -15,3 +20,17 @@ def search(rs: searchCriteria):
 @router.post("/chat")
 async def search(message_payload: ChatPayload):
     return await wsservice.chat(message_payload)
+<<<<<<< HEAD:wrapperfunction/website/ctrl/ws_ctrl.py
+=======
+# The following code creates an endpoint to get chat history by User ID
+# This handles fastAPI routing, using the cosmosDB to interact with the database.
+@router.get("/chat/history/{user_id}")
+async def get_chat_history(user_id: str):
+    try:
+        chat_history = db_connector.get_chat_history_by_user_id(user_id)
+        if not chat_history:
+            raise HTTPException(status_code=404, detail="Chat history not found")
+        return chat_history
+    except Exception as error:
+        return {"error": True, "message": str(error)}
+>>>>>>> 4a5de49 (adding endpoint to get chat history):api/wrapperfunction/website/ctrl/ws_ctrl.py
