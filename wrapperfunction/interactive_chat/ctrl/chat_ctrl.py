41c308cd517de764e2282c8ae87b10d9f6d83a5e
AsimQarawi <129060127+AsimQarawi@users.noreply.github.com>
2024-11-05 15:02:11 +0300

    change user, manger to one controller

--- wrapperfunction/interactive_chat/ctrl/chat_ctrl.py
+++ wrapperfunction/interactive_chat/ctrl/chat_ctrl.py
@@ -1,37 +1,34 @@
 from fastapi import APIRouter
 from wrapperfunction.interactive_chat.model.interactive_chat_model import GetForm, Prompt, Status, SubmitForm
-from wrapperfunction.interactive_chat.service import manager_service, user_service
+from wrapperfunction.interactive_chat.service import chat_service
 
 router = APIRouter()
 
-@router.post("/user_chat")
-async def user_chat(request: Prompt):
-    return await user_service.user_chat(request.prompt)
+@router.post("/chat")
+async def chat(request: Prompt):
+    return await chat_service.active_chat(request.prompt)
 
-@router.post("/user_chat/submit_vacation_form")
-async def user_submit_form(request: SubmitForm):
-    return await user_service.user_submit_form(request.form)
+#### ACTION ENDPOINTS ####
+@router.post("/chat/submit")
+async def submit_form(request: SubmitForm):
+    return await chat_service.submit_form(request.form)
 
-@router.post("/manager_chat")
-async def manager_chat(request: Prompt):
-    return await manager_service.manager_chat(request.prompt)
+@router.post("/chat/approve")
+async def approve_action(arguments:Status):
+    return await chat_service.approve_action(arguments)
 
-@router.post("/manager_chat/approve_vacation")
-async def manager_approve_action(arguments:Status):
-    return await manager_service.manager_approve_action(arguments)
+@router.post("/chat/disapprove")
+async def disapprove_action(arguments:Status):
+    return await chat_service.disapprove_action(arguments)
 
-@router.post("/manager_chat/disapprove_vacation")
-async def manager_disapprove_action(arguments:Status):
-    return await manager_service.manager_disapprove_action(arguments)
+@router.post("/chat/pending")
+async def pending_action(arguments:Status):
+    return await chat_service.pending_action(arguments)
 
-@router.post("/manager_chat/pending_vacation")
-async def manager_pending_action(arguments:Status):
-    return await manager_service.manager_pending_action(arguments)
-
-@router.get("/manager_chat/filter_vacation_forms_by")
-async def manager_getForm_action(arguments: GetForm):
-    return await manager_service.manager_getForm_action(arguments)
+@router.get("/chat/filter_vacation_forms_by")
+async def getForm_action(arguments: GetForm):
+    return await chat_service.getForm_action(arguments)
     
-@router.get("/manager_chat/get_all_vacation_forms")
-async def manager_getAllForms_action():
-    return await manager_service.manager_getAllForms_action() 
+@router.get("/chat/get_all_vacation_forms")
+async def getAllForms_action():
+    return await chat_service.getAllForms_action() 
