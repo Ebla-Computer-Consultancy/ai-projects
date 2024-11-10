1718976b20abc4a561ee1a92398c66bcba0e5148
AsimQarawi <129060127+AsimQarawi@users.noreply.github.com>
2024-11-10 19:04:49 +0300

    add media search

--- wrapperfunction/admin/model/crawl_model.py
+++ wrapperfunction/admin/model/crawl_model.py
@@ -1,4 +1,11 @@
 from pydantic import BaseModel
 
 # class CrawlRequest(BaseModel):
-#     new_content_file:  
+#     new_content_file:
+
+class MediaRequest(BaseModel):
+    search_text: str 
+
+class MediaCrawlRequest(BaseModel):
+    topic: str
+    url: str 
