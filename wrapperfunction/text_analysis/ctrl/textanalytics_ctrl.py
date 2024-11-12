549427c385286efff81552c4d1cf16a9c9d2e2ca
baheraser123456 <123304069+baheraser123456@users.noreply.github.com>
2024-11-12 18:55:30 +0200

    perform semantic analysis in all conversations

--- wrapperfunction/text_analysis/ctrl/textanalytics_ctrl.py
+++ wrapperfunction/text_analysis/ctrl/textanalytics_ctrl.py
@@ -3,8 +3,8 @@ from fastapi import APIRouter
 import wrapperfunction.text_analysis.service.text_analysis_service as text_analysis_service 
 router = APIRouter()
 @router.post("/semantic_analysis/")
-def apply_analysis(conv_id: str):
-    return text_analysis_service.perform_semantic_analysis(conv_id)
+def apply_analysis():
+    return text_analysis_service.perform_semantic_analysis()
 @router.post("/add_feedback/")
 def add_feedback(conv_id: str, feedback: int):
     return text_analysis_service.perform_feedback_update(conv_id, feedback)
--- wrapperfunction/text_analysis/service/text_analysis_service.py
+++ wrapperfunction/text_analysis/service/text_analysis_service.py
@@ -3,18 +3,18 @@ from wrapperfunction.chat_history.service import chat_history_service
 from fastapi import HTTPException
 import wrapperfunction.text_analysis.integration.textanalytics_connector as text_connector
 
-def perform_semantic_analysis(conversation_id: str):
+def perform_semantic_analysis():
     try:
-        messages = chat_history_service.get_chat_history(conversation_id)
-        message_texts = [msg["content"] for msg in messages if "content" in msg]
-        if not message_texts:
-            raise HTTPException(status_code=400, detail="No valid messages found for semantic analysis.")
-        semantic_data = text_connector.analyze_sentiment(message_texts)
-        print(semantic_data)
-        chat_history_service.update_conversation(conversation_id, {"sentiment": semantic_data})
-        
-        return semantic_data  
-
+        conversations=chat_history_service.get_all_conversations()
+        for conversation in conversations:
+            if conversation["sentiment"] is "":
+                conversation_id = conversation["conversation_id"]
+                messages = chat_history_service.get_chat_history(conversation_id)
+                message_texts = [msg["content"] for msg in messages if "content" in msg]
+                if not message_texts:
+                    raise HTTPException(status_code=400, detail="No valid messages found for semantic analysis.")
+                semantic_data = text_connector.analyze_sentiment(message_texts)
+                chat_history_service.update_conversation(conversation_id, {"sentiment": semantic_data}) 
     except Exception as e:
         raise HTTPException(status_code=400, detail=str(e))
 
