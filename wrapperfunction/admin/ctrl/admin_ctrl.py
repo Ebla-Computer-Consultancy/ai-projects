7741faecfa2621677377bc8ce07ad4dcc709fe80
alaa-alnissany <64522552+alaa-alnissany@users.noreply.github.com>
2024-11-13 13:04:49 +0300

    --add new features and there is somthing need to fix

--- wrapperfunction/admin/ctrl/admin_ctrl.py
+++ wrapperfunction/admin/ctrl/admin_ctrl.py
@@ -1,10 +1,7 @@
 from fastapi import APIRouter, HTTPException, Request
-# import wrapperfunction.admin.service.admin_service as adminservice
 from fastapi import HTTPException , File, Form
 from wrapperfunction.admin.model.indexer_model import IndexerRequest
 from wrapperfunction.admin.service import admin_service
-
-# import wrapperfunction.admin.model.crawl_model as CrawlRequest
 
 router = APIRouter()
 
@@ -15,19 +12,41 @@ async def crawl(request: Request):
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
-@router.post("/delete_subfolder/")
-async def delete_subfolder(request: Request):
+@router.post("/scrape")
+async def scrape(file_path: str, container_name:str):
     try:
         return await admin_service.delete_subfolder(request)
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
-@router.delete("/delete_blob/")
-async def delete_blob(request: Request):
+@router.post("/delete_subfolder")
+async def delete_subfolder(container_name:str, subfolder_name:str):
     try:
         return await admin_service.delete_blob(request)
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
+
+@router.delete("/delete_blob")
+async def delete_blob(metadata_key:str,metadata_value:str):
+    try:
+        return await adminservice.delete_blob(metadata_key,metadata_value)
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+    
+@router.delete("/reset_index")
+async def reset_index(index_name: str):
+    try:
+        return adminservice.delete_indexes(index_name,key ="chunk_id", value = None )
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+@router.delete("/reset_index")
+async def reset_index(index_name: str, value:str, key:str ="chunk_id"):
+    try:
+        return adminservice.delete_indexes(index_name,key,value)
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+    
 
 # @router.post("/edit_blob/")
 # async def edit_blob(metadata_key: str, metadata_value: str,
