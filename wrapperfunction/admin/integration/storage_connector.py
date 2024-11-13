1d7b3a97998e31abb4eea4272f2c3ebd4cb3649b
AsimQarawi <129060127+AsimQarawi@users.noreply.github.com>
2024-11-13 15:05:04 +0300

    convert data to json, uplade directly to container, add media setting json

--- wrapperfunction/admin/integration/storage_connector.py
+++ wrapperfunction/admin/integration/storage_connector.py
@@ -53,19 +53,16 @@ def delete_blob_snapshots(connection_string: str, container_name: str, blob_name
     )
     blob_client.delete_blob(delete_snapshots="include")
 
-def upload_file_to_azure(file_path, container_name, blob_name, connection_string):
-    with open(file_path, "rb") as data:
-        blob_service_client = BlobServiceClient.from_connection_string(connection_string)
-        blob_client = blob_service_client.get_blob_client(container=container_name, blob=blob_name)
-        blob_client.upload_blob(data)
-        print(f"Uploaded {file_path} to {blob_name}")
-           
-def push_To_Container(folder, connection_string, container_name):
-    for root, dirs, files in os.walk(folder):
-        for file in files:
-            local_file_path = os.path.join(root, file)
-                
-            relative_path = os.path.relpath(local_file_path, folder)
-            blob_name = relative_path.replace("\\", "/") 
+def upload_json_to_azure(content, container_name, blob_name, connection_string):
+    print(f"\nUploading ... file\n")
+    # Convert the text content to bytes
+    content_bytes = content.encode("utf-8")
 
-            upload_file_to_azure(local_file_path, container_name, blob_name, connection_string)
+    # Create a blob service client
+    blob_service_client = BlobServiceClient.from_connection_string(connection_string)
+    blob_client = blob_service_client.get_blob_client(container=container_name, blob=blob_name)
+
+    # Upload the content directly to Azure Blob
+    blob_client.upload_blob(BytesIO(content_bytes), overwrite=True)
+    print(f"\nUploaded text content to {blob_name}\n")
+           
