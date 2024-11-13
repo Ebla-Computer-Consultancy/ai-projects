92a257dcec4977ae17ecf676681d3d97eaa32d3e
AsimQarawi <129060127+AsimQarawi@users.noreply.github.com>
2024-11-13 15:05:04 +0300

    convert data to json, uplade directly to container, add media setting json

--- wrapperfunction/media_monitoring/ctrl/media_ctrl.py
+++ wrapperfunction/media_monitoring/ctrl/media_ctrl.py
@@ -1,20 +1,20 @@
 from fastapi import APIRouter, HTTPException, Request
 from fastapi import HTTPException , File, Form
-from wrapperfunction.admin.service import admin_service
 from wrapperfunction.media_monitoring.model.media_model import MediaCrawlRequest, MediaRequest
+from wrapperfunction.media_monitoring.service import media_service
 
 router = APIRouter()
 
-@router.post("/search")
+@router.post("/search/")
 async def media_search(request:MediaRequest):
     try:
-        return await admin_service.media_search(request.search_text)
+        return await media_service.media_search(request.search_text)
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
-@router.post("/crawl")   
+@router.post("/crawl/")   
 async def media_crawl(request:MediaCrawlRequest):
     try:
-        return await admin_service.media_crawl(request.topics,request.urls)
+        return await media_service.media_crawl(request.topics,request.urls)
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
