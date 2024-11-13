24c9a2a6e7a29fca0f7278b8e81bb2bb34c3ab45
alaa-alnissany <64522552+alaa-alnissany@users.noreply.github.com>
2024-11-13 13:04:49 +0300

    --add new features and there is somthing need to fix

--- wrapperfunction/core/utls/spiders/crawling_spider.py
+++ wrapperfunction/core/utls/spiders/crawling_spider.py
@@ -1,101 +1,129 @@
-import json
-from scrapy import Request
-from scrapy.spiders import CrawlSpider
+from scrapy import Request, crawler
+from scrapy.exceptions import IgnoreRequest
+from scrapy.linkextractors import LinkExtractor
+from scrapy.spiders import CrawlSpider, Rule                                                                                          
+import re
+import requests
 from scrapy.http import Request
-from wrapperfunction.admin.service.blob_service import append_blob
-from wrapperfunction.core import config
-from wrapperfunction.core.utls.helper import (
-    get_title,
-    process_text_name,
-)
+from shutil import which
+import scrapy
+class CustomDuplicateFilterMiddleware:
+    def __init__(self, crawler):
+        self.visited_urls = set()
+
+    @classmethod
+    def from_crawler(cls, crawler):
+        return cls(crawler)
+
+    def process_request(self, request, spider):
+        if request.url in self.visited_urls:
+            raise IgnoreRequest(f"Skipping duplicate URL: {request.url}")
+        self.visited_urls.add(request.url)
+        return None
+
+class CrawlingPagesSpider(CrawlSpider):
+    name = "eblapagecrawler"
+    def __init__(self, urls=None, *args, **kwargs):
+        super(CrawlingPagesSpider,self).__init__(*args, **kwargs)
+        self.start_urls = kwargs.pop('start_urls', [])
+        # self.start_urls = urls if urls else []
+        self.results =[]
+    def parse(self,response):
+        def remove_html_tags(text):
+            tag_pattern = re.compile(r'<.*?>')
+            cleaned_text = tag_pattern.sub('', text)
+
+            cleaned_text = re.sub(r'[\n\t]', ' ', cleaned_text)
+            cleaned_text = ' '.join(cleaned_text.split())
+            return cleaned_text
+        def get_title(url,title):
+            title = remove_html_tags(title)
+            if title == "":
+                title = url.split("/")[-1]
+            return title
+        
+        title = get_title(url=response.url,title=response.xpath('//head/title/text()').get())
+
+        yield{
+            "url": response.url,
+            'title': title,
+            'body': title+"\n"+remove_html_tags('\n'.join(response.xpath("//div[not(descendant::nav) and not(descendant::style) and not(descendant::script) and not(ancestor::header) and not(ancestor::footer)]//text()").extract()))
+            }
+    def start_requests(self):
+        for url in self.start_urls:
+            yield Request(url, callback=self.parse)
 
 
+# crawle every thing
 class CrawlingSpider(CrawlSpider):
-    name = "crawler"
+
+    name = "eblacrawler"
     custom_settings = {
-        "DOWNLOADER_MIDDLEWARES": {
-            "scrapy.downloadermiddlewares.offsite.OffsiteMiddleware": None,
-        },
-        "USER_AGENT": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3",
-    }
-
-    def __init__(self, *args, **kwargs):
-        super(CrawlingSpider, self).__init__(*args, **kwargs)
-
-        self.start_urls = kwargs.get("start_urls")
-        self.cookies = kwargs.get("cookies")
-        self.headers = kwargs.get("headers")
-        self.allowed_domains = [
-            x.replace("https://", "").split("/")[0] for x in self.start_urls
-        ]
-
+        'DOWNLOADER_MIDDLEWARES': {
+            'scrapy.downloadermiddlewares.offsite.OffsiteMiddleware': None,
+           },
+        }
+    
     def _build_request(self, rule_index, link):
         return Request(
             url=link.url,
             callback=self._callback,
-            cookies=self.cookies,
-            headers=self.headers,
+            cookies={"LangSwitcher_Setting": "ar-SA"},
             errback=self._errback,
             meta=dict(
                 rule=rule_index,
                 link_text=link.text,
             ),
         )
+    
+    def __init__(self, *args, **kwargs): 
+      super(CrawlingSpider, self).__init__(*args, **kwargs) 
 
-    def parse_items(self, response):
-        # crawling document link
-        document_links = response.xpath(
-            '//a[contains(@href, ".pdf")]/@href | //a[contains(@data-pdf, ".pdf")]/@data-pdf'
-        ).getall()
+      self.start_urls = kwargs.pop('start_urls')[0].split(',')
+      print(self.start_urls)
+      sub ="https://"
+      self.allowed_domains = [x.replace(sub, '') for x in self.start_urls]
+      sub ="www."
+      self.allowed_domains = [x.replace(sub, '') for x in self.allowed_domains]
+      # print("------------------------------------",self.allowed_domains,"------------------------------------")
+
+    rules = (        
+        Rule(LinkExtractor(),callback="parse_item",follow=True),     
+    )
+    
+    def parse_item(self,response):
+        def remove_html_tags(text):
+            tag_pattern = re.compile(r"<.*?>")
+            cleaned_text = tag_pattern.sub("", text)
+
+            cleaned_text = re.sub(r"[\n\t]", " ", cleaned_text)
+            cleaned_text = " ".join(cleaned_text.split())
+            return cleaned_text
+
+        def get_title(url, title):
+            title = remove_html_tags(title)
+            if title == "":
+                title = url.split("/")[-1]
+            return title        
+        document_links = response.xpath('//a[contains(@href, ".pdf")]/@href | //a[contains(@data-pdf, ".pdf")]/@data-pdf').getall()# '//a[contains(@data-pdf, ".pdf")]/@data-pdf'
         if document_links:
             for link in document_links:
                 full_url = response.urljoin(link)
-                blob_name = f"item_{process_text_name(full_url)}.json"
-                data = {
-                    "pdf_url": full_url,
-                    "title": full_url.split("/")[-1],
-                }
-                data = json.dumps(data, ensure_ascii=False)
-                append_blob(
-                    folder_name=config.SUBFOLDER_NAME,
-                    blob_name=blob_name,
-                    blob=data,
-                    metadata_1=full_url[:-1],
-                    metadata_2=full_url,
-                    metadata_3="crawled",
-                    metadata_4="pdf",
-                )
-
-        # crawling page information
-        url = response.url
-        blob_name = f"item_{process_text_name(url)}.json"
-        ar_title = get_title(
-            url=response.url,
-            title=response.xpath("//head/title/text()").get(),
-        )
-        ar_body = (
-            ar_title
-            + " ".join(
-                "\n".join(
-                    response.xpath(
-                        "//body//*[not(self::script) and not(self::style) and not(self::link) and not(self::meta) and not(self::a)]/text()"
-                    ).getall()
-                ).split()
-            ).strip()
-        )
-        data = {"url": url, "title": ar_title, "body": ar_body}
-        data = json.dumps(data, ensure_ascii=False)
-        append_blob(
-            folder_name=config.SUBFOLDER_NAME,
-            blob_name=blob_name,
-            blob=data,
-            metadata_1=url[:-1],
-            metadata_2=url,
-            metadata_3="crawled",
-            metadata_4="link",
-        )
-
-
+                # full_url = "https://www.km.qa"+link
+                # pdf_response = requests.get(full_url)
+                yield{"pdf_url": full_url,"title":full_url.split('/')[-1]}#, "body": pdf_response.content}
+        
+        url =  response.url
+        ar_title = get_title(url=response.url,title=response.xpath('//head/title/text()').get())
+        ar_body= ar_title+"\n"+remove_html_tags('\n'.join(response.xpath("//div[not(descendant::nav) and not(descendant::style) and not(descendant::script) and not(ancestor::header) and not(ancestor::footer)]//text()").extract()))
+        yield {
+                 'url': url,
+                 'title': ar_title,
+                 'body': ar_body
+             }
+            
+        for link in response.xpath('*//a/@href').getall():
+            yield response.follow(link, self.parse)
 """
     def download_document(self, url,file_path):
         with open("./pdfs.txt", 'wb') as f:
