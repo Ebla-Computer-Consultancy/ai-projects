077d635276dbf0e298e25097787339e20412997c
abdulaziz-eblacorp <ashahrour@eblacorp.com>
2024-11-14 14:43:14 +0300

    fix -- some params

--- wrapperfunction/admin/ctrl/admin_ctrl.py
+++ wrapperfunction/admin/ctrl/admin_ctrl.py
@@ -1,52 +1,69 @@
-from fastapi import APIRouter, HTTPException, Request
+from typing import List
+from fastapi import APIRouter, HTTPException, Request, UploadFile
 import wrapperfunction.admin.service.admin_service as adminservice
-from fastapi import HTTPException , File, Form
+from fastapi import HTTPException
+
 # import wrapperfunction.admin.model.crawl_model as CrawlRequest
 
 router = APIRouter()
 
-@router.post("/crawl/")
-async def crawl(request: Request):
+
+# new crawling request
+# @router.get("/crawl")
+# async def crawl(request: Request, urls: List[str], deepCrawling: bool = True):
+#     try:
+#         return {"urls": urls, "deep-crawling": deepCrawling, "request": request}
+#     except Exception as e:
+#         raise HTTPException(status_code=500, detail=str(e))
+
+
+@router.post("/crawl")
+async def crawl(link: str):
     try:
-        return adminservice.crawl(request)
+        return adminservice.crawl(link)
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
+
 
 @router.post("/scrape")
-async def scrape(file_path: str, container_name:str):
+async def scrape(file: UploadFile, container_name: str):
     try:
-        return adminservice.scrape(file_path,container_name)
+        return adminservice.scrape(file, container_name)
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
+
 @router.post("/delete_subfolder")
-async def delete_subfolder(container_name:str, subfolder_name:str):
+async def delete_subfolder(container_name: str, subfolder_name: str):
     try:
         return await adminservice.delete_subfolder(container_name, subfolder_name)
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
+
 @router.delete("/delete_blob")
-async def delete_blob(metadata_key:str,metadata_value:str):
+async def delete_blob(metadata_key: str, metadata_value: str):
     try:
-        return await adminservice.delete_blob(metadata_key,metadata_value)
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
-    
-@router.delete("/reset_index")
-async def reset_index(index_name: str):
-    try:
-        return adminservice.delete_indexes(index_name,key ="chunk_id", value = None )
+        return await adminservice.delete_blob(metadata_key, metadata_value)
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
+
 @router.delete("/reset_index")
-async def reset_index(index_name: str, value:str, key:str ="chunk_id"):
+async def reset_index(index_name: str):
     try:
-        return adminservice.delete_indexes(index_name,key,value)
+        return adminservice.delete_indexes(index_name, key="chunk_id", value=None)
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
-    
+
+
+@router.delete("/reset_index")
+async def reset_index(index_name: str, value: str, key: str = "chunk_id"):
+    try:
+        return adminservice.delete_indexes(index_name, key, value)
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
 
 # @router.post("/edit_blob/")
 # async def edit_blob(metadata_key: str, metadata_value: str,
@@ -56,7 +73,7 @@ async def reset_index(index_name: str, value:str, key:str ="chunk_id"):
 #         return adminservice.edit_blob(new_content_file,metadata_key,metadata_value)
 #     except Exception as e:
 #         raise HTTPException(status_code=500, detail=str(e))
-    
+
 # @router.post("/add_pdfs/")
 # async def add_pdfs(request: Request):
 #     try:
--- wrapperfunction/admin/service/admin_service.py
+++ wrapperfunction/admin/service/admin_service.py
@@ -1,26 +1,46 @@
-from wrapperfunction.admin.integration.crawl_integration import run_crawler, process_and_upload, upload_files_to_blob,delete_blobs_base_on_metadata, delete_base_on_subfolder, edit_blob_by_new_jsonfile, split_json_file, update_json_with_pdf_text, scrape_csv
+from wrapperfunction.admin.integration.crawl_integration import (
+    run_crawler,
+    process_and_upload,
+    upload_files_to_blob,
+    delete_blobs_base_on_metadata,
+    delete_base_on_subfolder,
+    edit_blob_by_new_jsonfile,
+    split_json_file,
+    update_json_with_pdf_text,
+    scrape_csv,
+)
 from wrapperfunction.admin.integration.aisearch_connector import delete_indexed_data
 from wrapperfunction.core.utls.helper import download_pdfs
-from fastapi import HTTPException
+from fastapi import HTTPException, UploadFile
 from fastapi.responses import JSONResponse
 import json
 from urllib.parse import unquote
 
-def crawl(request):
-    link = request.query_params.get("link")
-    if not link:
-        raise ValueError("Missing link parameter")
 
-    filepath, filename = run_crawler(link) 
-    # filepath = "./export_e7ce4d72-a094-48ef-958c-647e61e59d17.json"
-    # split_json_file(filepath, filepath[:-5]+"-links.json", filepath[:-5]+"-docs.json")
-    # download_pdfs(filepath[:-5]+"-docs.json", "./export/docs/"+filepath[2:-5])
-    # update_json_with_pdf_text(filepath[:-5]+"-docs.json", "./export/docs/"+filepath[2:-5])
-    # process_and_upload(filepath[:-5]+"-links.json","./export/", link)
-    # process_and_upload("updated_"+filepath[2:-5]+"-docs.json","./export/docs/", link,True)
+def crawl(link: str):
+    filepath, filename = run_crawler(link)
+
+    split_json_file(
+        filepath, filepath[:-5] + "-links.json", filepath[:-5] + "-docs.json"
+    )
+    download_pdfs(filepath[:-5] + "-docs.json", "./export/docs/" + filepath[2:-5])
+    update_json_with_pdf_text(
+        filepath[:-5] + "-docs.json", "./export/docs/" + filepath[2:-5]
+    )
+    process_and_upload(filepath[:-5] + "-links.json", "./export/", link)
+    process_and_upload(
+        "updated_" + filepath[2:-5] + "-docs.json", "./export/docs/", link, True
+    )
     return {"message": "Crawling completed successfully"}
 
-async def delete_blob(metadata_key,metadata_value):
+
+def scrape(file: UploadFile, container_name: str):
+    results = scrape_csv(file, container_name)
+    process_and_upload("./" + container_name + ".json", "./export/", "  ")
+    return results
+
+
+async def delete_blob(metadata_key, metadata_value):
     if not metadata_key or not metadata_value:
         raise HTTPException(status_code=400, detail="Missing required parameters")
     try:
@@ -55,27 +75,24 @@ async def delete_subfolder(container_name, subfolder_name):
         raise HTTPException(status_code=404, detail="Subfolder not found")
 
 
-def edit_blob(new_content_file,metadata_key,metadata_value):
+def edit_blob(new_content_file, metadata_key, metadata_value):
     try:
         new_content = json.load(new_content_file.file)  #
         new_content_file.file.seek(0)
         print(new_content_file.file.read())
     except Exception as e:
-        raise HTTPException(status_code=500, detail=f"Failed to read new content file: {str(e)}")
-    edit_blob_by_new_jsonfile(metadata_key,metadata_value,new_content)
-    
+        raise HTTPException(
+            status_code=500, detail=f"Failed to read new content file: {str(e)}"
+        )
+    edit_blob_by_new_jsonfile(metadata_key, metadata_value, new_content)
 
-def delete_indexes(index_name: str, key:str, value):
+
+def delete_indexes(index_name: str, key: str, value):
     try:
         delete_indexed_data(index_name, key, value)
-        return JSONResponse(content={"message": f"index '{index_name} deleted successfully."}, status_code=200)
+        return JSONResponse(
+            content={"message": f"index '{index_name} deleted successfully."},
+            status_code=200,
+        )
     except:
         raise HTTPException(status_code=404, detail="index not found")
-
-def scrape(file_path: str,container_name):
-    if file_path.endswith('.csv'):
-        results = scrape_csv(file_path,container_name)
-        process_and_upload("./"+container_name+".json","./export/", "  ")
-        return results
-    else:
-        raise HTTPException(status_code=400, detail="Please provide a valid CSV file path.")
