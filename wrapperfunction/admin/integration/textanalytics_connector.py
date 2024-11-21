a34a4e04572d0b4cb445dbf666fbe10930d1b786
AsimQarawi <129060127+AsimQarawi@users.noreply.github.com>
2024-11-21 12:39:52 +0300

    add rera-media chatbot, edit textanalytics

--- wrapperfunction/admin/integration/textanalytics_connector.py
+++ wrapperfunction/admin/integration/textanalytics_connector.py
@@ -8,7 +8,13 @@ from azure.ai.textanalytics import TextAnalyticsClient
 text_analytics_client = TextAnalyticsClient(endpoint=config.AZURE_TEXT_ANALYTICS_ENDPOINT, credential=AzureKeyCredential(config.AZURE_TEXT_ANALYTICS_KEY))
 def analyze_sentiment(messages: list):
     try:
-        sentiment_response = text_analytics_client.analyze_sentiment(messages)[0]
+        detected_language_response = text_analytics_client.detect_language(messages)
+        language_code = detected_language_response[0].primary_language.iso6391_name
+
+        language = "ar" if language_code == "(Unknown)" else language_code
+        sentiment_response = text_analytics_client.analyze_sentiment(messages, language=language)[0]    
+
+
         return sentiment_response.sentiment  
     except Exception as e:
         raise HTTPException(status_code=400, detail=f"Error in sentiment analysis: {str(e)}")
