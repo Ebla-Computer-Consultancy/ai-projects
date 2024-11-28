2e25512350a555cf93ec079f8977ca79e7fd6c7a
alaa-alnissany <64522552+alaa-alnissany@users.noreply.github.com>
2024-11-28 20:25:35 +0300

    --add upload blobs - delete blobs - get blobs name -run index -reset index -index info

--- wrapperfunction/admin/service/admin_service.py
+++ wrapperfunction/admin/service/admin_service.py
@@ -3,68 +3,13 @@ import requests
 from fastapi import HTTPException
 from fastapi.responses import JSONResponse
 from wrapperfunction.admin.integration.aisearch_connector import delete_indexed_data
-from wrapperfunction.admin.integration.crawl_integration import delete_base_on_subfolder, delete_blobs_base_on_metadata, edit_blob_by_new_jsonfile, process_and_upload, run_crawler, transcript_pdfs
-from wrapperfunction.core.config import OPENAI_API_VERSION, OPENAI_CHAT_MODEL, RERA_STORAGE_CONNECTION, SEARCH_ENDPOINT, SEARCH_KEY
+from wrapperfunction.core.config import OPENAI_API_VERSION, SEARCH_ENDPOINT, SEARCH_KEY
 from wrapperfunction.core.model.service_return import ServiceReturn, StatusCode
+from fastapi import HTTPException
+from wrapperfunction.admin.integration.crawl_integration import run_crawler
+from wrapperfunction.admin.integration.aisearch_connector import delete_indexed_data, get_index_info,reset_indexed_data,run_indexer
+from fastapi.responses import JSONResponse
 
-
-def crawling(
-    urls: list[str],
-    headers: dict = None,
-    cookies: dict = None,
-    deepCrawling: bool = False,
-):
-    run_crawler(urls, headers, cookies, deepCrawling)
-    return "urls crawled successfully"
-
-
-async def delete_blob(metadata_key, metadata_value):
-    if not metadata_key or not metadata_value:
-        raise HTTPException(status_code=400, detail="Missing required parameters")
-    try:
-        print(metadata_value)
-        metadata_value_ = unquote(metadata_value)
-        print(metadata_value_)
-        delete_blobs_base_on_metadata(metadata_key, metadata_value)
-        return JSONResponse(
-            content={
-                "message": f"Blob with metadata {metadata_key}={metadata_value} deleted successfully"
-            },
-            status_code=200,
-        )
-    except:
-        raise HTTPException(status_code=404, detail="Blob not found")
-
-async def delete_subfolder(request):
-    container_name = request.query_params.get('container_name')
-    subfolder_name = request.query_params.get('subfolder_name')
-
-    if not container_name or not subfolder_name:
-        raise HTTPException(status_code=400, detail="Missing required parameters")
-    try:
-        print(container_name)
-        print(subfolder_name)
-        delete_base_on_subfolder(subfolder_name, container_name)
-        return JSONResponse(
-            content={
-                "message": f"Subfolder '{subfolder_name}' in the container {container_name} deleted successfully."
-            },
-            status_code=200,
-        )
-    except:
-        raise HTTPException(status_code=404, detail="Subfolder not found")
-
-
-def edit_blob(new_content_file, metadata_key, metadata_value):
-    try:
-        new_content = json.load(new_content_file.file)  #
-        new_content_file.file.seek(0)
-        print(new_content_file.file.read())
-    except Exception as e:
-        raise HTTPException(
-            status_code=500, detail=f"Failed to read new content file: {str(e)}"
-        )
-    edit_blob_by_new_jsonfile(metadata_key, metadata_value, new_content)
 
 
 def delete_indexes(index_name: str, key: str, value):
@@ -106,3 +51,24 @@ async def runIndexer(name: str):
                              ).to_dict()
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
+
+def reset_index(index_name: str):
+    try:
+        reset_indexed_data(index_name)
+        return JSONResponse(content={"message": f"index '{index_name} deleted successfully."}, status_code=200)
+    except:
+        raise HTTPException(status_code=404, detail="index not found")
+
+def run_index(index_name: str):
+    try:
+        run_indexer(index_name)
+        return JSONResponse(content={"message": f"index '{index_name} reloaded successfully."}, status_code=200)
+    except:
+        raise HTTPException(status_code=404, detail="index not found")
+    
+def index_info(index_name: str):
+    try:
+        return get_index_info(index_name)
+    except:
+        raise HTTPException(status_code=404, detail="index not found")
+
