1ccafaa21bec25a3d91a8dedbd8949e5b56ce92d
baheraser123456 <123304069+baheraser123456@users.noreply.github.com>
2024-12-05 16:20:16 +0200

    fix some issues

--- wrapperfunction/document_intelligence/ctrl/document_intelligence_ctrl.py
+++ wrapperfunction/document_intelligence/ctrl/document_intelligence_ctrl.py
@@ -17,10 +17,10 @@ def document_intelligence(model_id: str = "prebuilt-layout", file: UploadFile = 
     )
 
 
-@router.post("/uploadfile/")
+@router.post("/auto-split-pdf/")
 async def upload_file(file: UploadFile ,criteria:Optional[str] = None):
     try:
-        res=documentintelligenceservice.split_pdf(file.file, criteria)
+        res=documentintelligenceservice.auto_split_pdf(file.file, criteria)
         return res
     except Exception as e:
         return {"status": "error", "message": str(e)}
--- wrapperfunction/document_intelligence/service/document_intelligence_service.py
+++ wrapperfunction/document_intelligence/service/document_intelligence_service.py
@@ -1,4 +1,5 @@
 
+import ast
 from io import BytesIO
 import json
 import os
@@ -65,18 +66,18 @@ def get_openai_instruction(prompt):
 def detection(text,criteria:Optional[str] = None):
     if not criteria:
         prompt = (
-        "The following text is extracted from file. Identify the best practice to split this file. "
-        "Return the results as a list of page numbers without any other text:\n\n"
-        f"{text}"
-    )
+            "The following text is extracted from file. Identify the best practice to split this file. "
+            "Return the results as a Python list of page number ranges without any extra text. Example: [[1, 10], [11, 20]].\n\n"
+            f"{text}"
+        )
     else:
-        print(criteria)
         prompt = (
-    f"The following text is extracted from file, Identify the best practice to split this file based on {criteria}. "
-    "Return the results as a list of page numbers without any other text. "
-    "If no criteria is specified, return an empty list:\n\n"
+    f"The following text is extracted from a file. Split the file based on the following criteria: {criteria}. "
+    "If splitting based on this criteria is not possible, identify the best practice for splitting the file. "
+    "Return the results as a Python list of page number ranges (e.g., [[1, 10], [11, 20]])with no additional text. "
+    "If no split is possible, return an empty list `[]` with no additional text.\n\n"
     f"{text}"
-)   
+)
     try:
         response = openaiconnector.client.chat.completions.create( 
             model=config.OPENAI_CHAT_MODEL, 
@@ -85,39 +86,37 @@ def detection(text,criteria:Optional[str] = None):
                 {"role": "user", "content": prompt} 
             ], 
             max_tokens=200, 
-            temperature=0.5, 
+            temperature=0.4, 
         )
-        content = response.choices[0].message.content       
-        return list(eval(content))
+        content = response.choices[0].message.content  
+        return list(ast.literal_eval(content))
     except Exception as e:
         return f"Error with Azure OpenAI API: {e}"
 
 
 
-def split_pdf(input_pdf_path, criteria):
+def auto_split_pdf(pdf, criteria):
     try:
-        reader = PdfReader(input_pdf_path)
+        reader = PdfReader(pdf)
         text_per_page = [page.extract_text() for page in reader.pages]
         full_text = "\n".join(f"Page {i+1}:\n{text}" for i, text in enumerate(text_per_page))
-
 
         pages = detection(full_text, criteria)
 
 
-        pages.append(len(reader.pages) + 1)
+        pages.append([len(reader.pages) + 1])  
         files = []
 
         for i in range(len(pages) - 1):
             writer = PdfWriter()
-            for j in range(pages[i] - 1, pages[i + 1] - 1):
-                writer.add_page(reader.pages[j])
 
+            for j in range(pages[i][0] - 1, pages[i + 1][0] - 1):
+                writer.add_page(reader.pages[j])
 
             output_file_path = f"{i + 1}.pdf"
             with open(output_file_path, "wb") as output_file:
                 writer.write(output_file)
             files.append(output_file_path)
-
 
         zip_buffer = BytesIO()
         with zipfile.ZipFile(zip_buffer, 'w', zipfile.ZIP_DEFLATED) as zip_file:
@@ -125,11 +124,12 @@ def split_pdf(input_pdf_path, criteria):
                 zip_file.write(file, os.path.basename(file))
                 os.remove(file) 
         zip_buffer.seek(0)
-        container_client = storageconnector.get_blob_service_client(config.SPLITER_CONTAINER_NAME)
-        response=container_client.upload_blob(f"{uuid.uuid4()}.zip",zip_buffer, overwrite=True)
 
-        return {"status": "success", "message": "Zip file uploaded successfully","url":f"{response.url}"}
+
+        container_client = storageconnector.get_blob_service_client(config.SPLITER_CONTAINER_NAME)
+        response = container_client.upload_blob(f"{uuid.uuid4()}.zip", zip_buffer, overwrite=True)
+
+        return {"status": "success", "message": "Zip file uploaded successfully", "url": f"{response.url}", "ranges": f"{pages}"}
 
     except Exception as e:
         return {"status": "error", "message": str(e)}
-
