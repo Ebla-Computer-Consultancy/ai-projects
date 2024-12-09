617b5095663cfea2e34ff556223f4d04da8c969c
AsimQarawi <129060127+AsimQarawi@users.noreply.github.com>
2024-12-09 09:49:03 +0300

    pending state and action final msg

--- wrapperfunction/interactive_chat/model/interactive_model.py
+++ wrapperfunction/interactive_chat/model/interactive_model.py
@@ -87,7 +87,7 @@ class VacationFormEntity:
             "Start_Date":self.start_date,
             "End_Date":self.end_date,
             "Total_Days":self.total_days,
-            "Status":FormStatus.PENDING,
+            "Status":FormStatus.PENDING.value,
             "Comments":self.comments
         }
 class Status(BaseModel):
