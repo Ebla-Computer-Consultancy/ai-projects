9b5b5e4148fdf6cbd3d632f5fe54ede6269a6e96
AsimQarawi <129060127+AsimQarawi@users.noreply.github.com>
2025-01-05 16:41:47 +0300

    edit jwt generation steps

--- wrapperfunction/function_auth/service/jwt_service.py
+++ wrapperfunction/function_auth/service/jwt_service.py
@@ -8,11 +8,20 @@ from wrapperfunction.function_auth.model.func_auth_model import User
 # Function to create JWT tokens
 def generate_jwt_tokens(user: User) -> Tuple[str, str]:
     try:
-        # Current time
-        now = datetime.utcnow()
-
         # Create Access Token (1-hour expiration)
-        access_token = jwt.encode(
+        access_token = generate_access_token(user)
+
+        # Create Refresh Token (24-hour expiration)
+        refresh_token = generate_refresh_token(user)
+
+        return access_token, refresh_token
+    except Exception as e:
+        raise Exception("Error While Generating Tokens")
+
+def generate_access_token(user: User) -> str:
+    now = datetime.utcnow()
+    # Create Access Token (1-hour expiration)
+    access_token = jwt.encode(
             payload = {
                 "sub": user.username,
                 "permissions": user.permissions,
@@ -22,22 +31,23 @@ def generate_jwt_tokens(user: User) -> Tuple[str, str]:
             key = config.JWT_SECRET_KEY,
             algorithm=config.ALGORITHM
         )
+    return access_token
 
-        # Create Refresh Token (24-hour expiration)
-        refresh_token = jwt.encode(
+def generate_refresh_token(user: User) -> str:
+    now = datetime.utcnow()
+    # Create Refresh Token (24-hours expiration)
+    refresh_token = jwt.encode(
             payload = {
                 "sub": user.username,
                 "permissions": user.permissions,
                 "enc_password": user.enc_password, 
-                "exp": now + timedelta(hours=24)  # Refresh token expires in 24 hours
+                "exp": now + timedelta(hours=24)
             },
             key = config.JWT_SECRET_KEY,
             algorithm = config.ALGORITHM
         )
+    return refresh_token
 
-        return access_token, refresh_token
-    except Exception as e:
-        raise Exception("Error While Generating Tokens")
 # Function to decode and verify JWT tokens
 def decode_jwt(token: str):
     try:
