77bc28605b672cf8433b31ba4992e7be00e3b2d4
AsimQarawi <129060127+AsimQarawi@users.noreply.github.com>
2025-01-20 12:35:10 +0300

    edit settings

--- wrapperfunction/core/service/settings_service.py
+++ wrapperfunction/core/service/settings_service.py
@@ -41,9 +41,9 @@ async def add_setting(entity):
     except Exception as e:
         return HTTPException(status_code=400, detail=str(e))
 
-def delete_bot_settings(partition_key,row_key):
+def delete_bot_settings(entity):
     try:
-        res =  db_connector.delete_entity(config.COSMOS_SETTINGS_TABLE,partition_key,row_key)
+        res =  db_connector.delete_entity(config.COSMOS_SETTINGS_TABLE,entity)
         return res
     except Exception as e:
         return HTTPException(status_code=400, detail=str(e))
