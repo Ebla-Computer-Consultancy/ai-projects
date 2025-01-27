e889f7913d4c71a4ef09f388909157b01daf606e
AsimQarawi <129060127+AsimQarawi@users.noreply.github.com>
2025-01-27 15:20:03 +0300

    edit error msg

--- wrapperfunction/function_auth/service/auth_db_service.py
+++ wrapperfunction/function_auth/service/auth_db_service.py
@@ -177,7 +177,7 @@ def validate_password(password: str):
     if not password.strip():
         return False, "Password cannot be empty."
     if len(password) < 8:
-        return False, "Password must be between 8 and 64 characters."
+        return False, "Password must be at least 8 characters"
     if not re.search(r'[A-Z]', password):
         return False, "Password must contain at least one uppercase letter."
     if not re.search(r'[a-z]', password):
