3e1f27b8a2255494053a14131a42450152e5126f
AsimQarawi <129060127+AsimQarawi@users.noreply.github.com>
2025-02-06 18:06:20 +0300

    edit level

--- function_app.py
+++ function_app.py
@@ -1,7 +1,7 @@
 import azure.functions as func
 from wrapperfunction.main import app as fastapi_app
 from wrapperfunction.media_monitoring.blueprint.media_blueprint import media_bp
-app = func.AsgiFunctionApp(app=fastapi_app, http_auth_level=func.AuthLevel.ANONYMOUS)
-# app = func.AsgiFunctionApp(app=fastapi_app, http_auth_level=func.AuthLevel.FUNCTION)
+# app = func.AsgiFunctionApp(app=fastapi_app, http_auth_level=func.AuthLevel.ANONYMOUS)
+app = func.AsgiFunctionApp(app=fastapi_app, http_auth_level=func.AuthLevel.FUNCTION)
 
 app.register_functions(media_bp)
