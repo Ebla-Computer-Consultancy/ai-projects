a21f5bdf00f6bf77cda569d61a7bc3e70abeb71b
baheraser123456 <123304069+baheraser123456@users.noreply.github.com>
2025-02-09 15:10:35 +0200

    edit func_app

--- function_app.py
+++ function_app.py
@@ -3,6 +3,6 @@ from wrapperfunction.main import app as fastapi_app
 from wrapperfunction.chat_history.bp.chat_history_blue_print import chat_history_bp
 
 
-app = func.AsgiFunctionApp(app=fastapi_app, http_auth_level=func.AuthLevel.ANONYMOUS)
-# app = func.AsgiFunctionApp(app=fastapi_app, http_auth_level=func.AuthLevel.FUNCTION)
+# app = func.AsgiFunctionApp(app=fastapi_app, http_auth_level=func.AuthLevel.ANONYMOUS)
+app = func.AsgiFunctionApp(app=fastapi_app, http_auth_level=func.AuthLevel.FUNCTION)
 app.register_functions(chat_history_bp)
