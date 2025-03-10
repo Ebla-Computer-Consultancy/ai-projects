29faf05b5a04d1db868535c5d8f6740ebd2eb74e
abdulaziz-eblacorp <ashahrour@eblacorp.com>
2025-03-10 13:14:02 +0300

    update auth level

--- function_app.py
+++ function_app.py
@@ -4,8 +4,8 @@ from wrapperfunction.media_monitoring.blueprint.media_blueprint import media_bp
 from wrapperfunction.chat_history.bp.chat_history_blue_print import chat_history_bp
 from wrapperfunction.social_media.blueprint.social_blueprint import social_bp
 
-app = func.AsgiFunctionApp(app=fastapi_app, http_auth_level=func.AuthLevel.ANONYMOUS)
-# app = func.AsgiFunctionApp(app=fastapi_app, http_auth_level=func.AuthLevel.FUNCTION)
+# app = func.AsgiFunctionApp(app=fastapi_app, http_auth_level=func.AuthLevel.ANONYMOUS)
+app = func.AsgiFunctionApp(app=fastapi_app, http_auth_level=func.AuthLevel.FUNCTION)
 
 app.register_functions(media_bp)
 app.register_functions(chat_history_bp)
