abacedf1455b58d6f01f76d654fd5ea93aa5a9d5
AsimQarawi <129060127+AsimQarawi@users.noreply.github.com>
2025-03-20 12:46:15 +0300

    handling to not add repeated tweets, add avatar configs

--- wrapperfunction/social_media/service/x_service.py
+++ wrapperfunction/social_media/service/x_service.py
@@ -35,6 +35,7 @@ def prepare_x_data_and_upload(results: dict):
     try:
         tweets = results["includes"]["tweets"]
         index = 0
+        media_info = config.get_media_info()
         for tweet in tweets:
             url = tweet["entities"].get("urls",[])
             page = {
@@ -42,28 +43,31 @@ def prepare_x_data_and_upload(results: dict):
                 "url": url[0]["url"] if len(url) > 0 else None,
                 "created_at": tweet["created_at"]
             }
-            media_info = config.get_media_info()
             blob_service.append_blob(blob=json.dumps(page,ensure_ascii=False),blob_name=f"{index}_tweet.json",folder_name=f"social_media/X/{datetime.datetime.now().date()}",container_name=media_info.container_name,metadata_2=page["url"])
             index += 1
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Error While Uploading X data: index-{index}:{str(e)}")
 
 async def add_x_data_to_knowledge_db(results: dict):
+    all_tweets = cosmos_db_connector.get_entities(config.X_TABLE)
+    existing_tweet_ids = {tweet["tweet_id"] for tweet in all_tweets} # Convert to set for faster searching
+    
     data = results["data"]
     for tweet in data:
-        tweet_data = get_author_data(tweet["author_id"], results)
-        tweet_data["text"] = tweet["text"]
-        tweet_data["created_at"] = tweet["created_at"]
-        tweet_data["tweet_id"] = tweet["id"]
-        tweet_data["PartitionKey"] = str(uuid.uuid4())
-        tweet_data["RowKey"] = str(uuid.uuid4())
-        tweet_data["language"] = textanalytics_connector.detect_language(messages=[tweet["text"]])[tak.LANGUAGE_ISO6391_NAME.value]
-        tweet_data["keyphrases"] = textanalytics_connector.extract_key_phrases(messages=[tweet["text"]],language=tweet_data["language"])
-        tweet_data["keyphrases"] = None if len(tweet_data["keyphrases"]) == 0 else ",".join(map(str, tweet_data["keyphrases"]))
-        tweet_data["sentiment"] = textanalytics_connector.analyze_sentiment(messages=[tweet["text"]])
-        if auth_service.exist_property(dic=tweet, field="referenced_tweets"):
-            tweet_data["referenced_tweets"] = get_ids_list(tweet["referenced_tweets"])
-        await cosmos_db_connector.add_entity(config.X_TABLE,tweet_data)
+        if tweet["id"] not in existing_tweet_ids:
+            tweet_data = get_author_data(tweet["author_id"], results)
+            tweet_data["text"] = tweet["text"]
+            tweet_data["created_at"] = tweet["created_at"]
+            tweet_data["tweet_id"] = tweet["id"]
+            tweet_data["PartitionKey"] = str(uuid.uuid4())
+            tweet_data["RowKey"] = str(uuid.uuid4())
+            tweet_data["language"] = textanalytics_connector.detect_language(messages=[tweet["text"]])[tak.LANGUAGE_ISO6391_NAME.value]
+            tweet_data["keyphrases"] = textanalytics_connector.extract_key_phrases(messages=[tweet["text"]],language=tweet_data["language"])
+            tweet_data["keyphrases"] = None if len(tweet_data["keyphrases"]) == 0 else ",".join(map(str, tweet_data["keyphrases"]))
+            tweet_data["sentiment"] = textanalytics_connector.analyze_sentiment(messages=[tweet["text"]])
+            if auth_service.exist_property(dic=tweet, field="referenced_tweets"):
+                tweet_data["referenced_tweets"] = get_ids_list(tweet["referenced_tweets"])
+            await cosmos_db_connector.add_entity(config.X_TABLE,tweet_data)
 
 def get_author_data(id: str, results: dict) -> dict:
     users = results["includes"]["users"]
@@ -83,4 +87,4 @@ def get_ids_list(nodes_list: list):
     ids_list = []
     for node in nodes_list:
         ids_list.append(node["id"])
-    return ",".join(map(str, ids_list))
+    return ",".join(map(str, ids_list))         
